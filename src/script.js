c59f16b25305c3e405887bed99e43f6fdd4904a3
michel-amorim <michelmsa7799@gmail.com>
2021-09-11 02:29:15 -0300

    fix: melhoramento codigo

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// Menu mobile
+
 const btnMobile = document.getElementById("btn-mobile");
 
 function toggleMenu(event) {
@@ -15,3 +17,13 @@ function toggleMenu(event) {
 
 btnMobile.addEventListener("click", toggleMenu);
 btnMobile.addEventListener("touchstart", toggleMenu);
+
+// production carrocel
+
+document.querySelector("#items").addEventListener("wheel", (event) => {
+  if (event.deltaY > 0) {
+    event.target.scrollBy(300, 0);
+  } else {
+    event.target.scrollBy(-300, 0);
+  }
+});
